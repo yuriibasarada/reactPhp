4c4b7a70dc0618b8478fb025b6bd84577d42526f
Yuriy Basarad <yuriyshkvarc@gmail.com>
2019-10-29 17:31:02 +0200

    to add a new changes for server.php file 29.10.2019

--- server.php
+++ server.php
@@ -1,25 +1,51 @@
 <?php
 
-use Psr\Http\Message\ServerRequestInterface;
+use App\Core\ErrorHandler;
+use App\Orders\Controller\CreateOrder;
+use App\Orders\Controller\DeleteOrder;
+use App\Orders\Controller\GetAllOrders;
+use App\Orders\Controller\GetOrderById;
+use App\Products\Controller\CreateProduct;
+use App\Products\Controller\DeleteProduct;
+use App\Products\Controller\GetAllProducts;
+use App\Products\Controller\GetProductById;
+use App\Products\Controller\UpdateProduct;
+use App\Core\Router;
+use FastRoute\DataGenerator\GroupCountBased;
+use FastRoute\RouteCollector;
+use FastRoute\RouteParser\Std;
 use React\EventLoop\Factory;
-use React\Http\Response;
 use React\Http\Server;
+
 
 require 'vendor/autoload.php';
 
 $loop = Factory::create();
 
-$server = new Server(function(ServerRequestInterface $request) {
-    return new Response(
-        200,
-        ['Context-Type' => 'application/json'],
-        json_encode(['message' => 'Hello'])
-    );
-});
+$routes = new RouteCollector(new Std(), new GroupCountBased());
+
+// Routes for products
+$routes->get('/products', new GetAllProducts());
+$routes->post('/products', new CreateProduct());
+$routes->get('/products/{id:\d+}', new GetProductById());
+$routes->put('/products/{id:\d+}', new UpdateProduct());
+$routes->delete('/products/{id:\d+}', new DeleteProduct());
+
+$routes->get('/orders', new GetAllOrders());
+$routes->post('/orders', new CreateOrder());
+$routes->get('/orders/{id:\d+}', new GetOrderById());
+$routes->delete('/orders/{id:\d+}', new DeleteOrder());
+
+$server = new Server([new ErrorHandler(), new Router($routes)]);
 
 $socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
 
 $server->listen($socket);
 
+$server->on('error', function (Throwable $error) {
+    echo 'Error: ' . $error->getMessage() . PHP_EOL;
+});
+
 echo 'Listening on ' . str_replace('tcp', 'http', $socket->getAddress()) . PHP_EOL;
+
 $loop->run();
