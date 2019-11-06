23ad0efad55be5f8352659af2ce1d6e56231327d
yuriy <yuriyshkvarc@gmail.com>
2019-11-06 15:06:17 +0200

    to add a new(last changes)

--- migrations.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-
-return [
-    'migrations_namespace' => 'App\Migrations',
-    'migrations_directory' => __DIR__ . '/Migrations',
-];
---
+++ src/Authentication/Input.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Authentication;
+
+
+use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Validator;
+
+final class Input
+{
+    /**
+     * @var ServerRequestInterface
+     */
+    private $request;
+
+    public function __construct(ServerRequestInterface $request)
+    {
+
+        $this->request = $request;
+    }
+
+    public function validate(): void
+    {
+        $emailValidator = Validator::key('email',
+            Validator::allOf(
+                Validator::email(),
+                Validator::notBlank(),
+                Validator::stringType()
+            ))->setName('email');
+
+        $passwordValidator = Validator::key('password',
+            Validator::allOf(
+                Validator::notBlank(),
+                Validator::stringType()
+            ))->setName('password');
+
+        $validator = Validator::allOf($emailValidator, $passwordValidator);
+
+        $validator->assert($this->request->getParsedBody());
+    }
+
+    public function hashedPassword(): string
+    {
+        return password_hash($this->request->getParsedBody()['password'],PASSWORD_BCRYPT);
+    }
+
+    public function email(): string
+    {
+        return  $this->request->getParsedBody()['email'];
+    }
+}
